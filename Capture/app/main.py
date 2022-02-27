fabc69140807ad11ac13c1ef4de360c5785b919a
Louis-Marie <louis-marie.restout@gmail.com>
2022-02-27 10:39:03 +0100

    Capture with realsense
    With good RGB values !

--- Capture/app/main.py
+++ Capture/app/main.py
@@ -1,18 +1,37 @@
-import cv2 as cv
-import sys
+import cv2                                # state of the art computer vision algorithms library
+import numpy as np                        # fundamental package for scientific computing
+import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
 
-cam = cv.VideoCapture(-1)
-if not cam.isOpened():
-    sys.exit("Cannot open camera")
+#-> Setup
+pipe = rs.pipeline()
+cfg = rs.config()
+cfg.enable_stream(rs.stream.color)
+profile = pipe.start(cfg)
+
+#-> Skip 5 first frames to give the Auto-Exposure time to adjust
+for _ in range(5):
+  pipe.wait_for_frames()
 
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        sys.exit("Not frame found")
-    
-    # ... 
-    cv.imshow("frame", frame)
+  #-> Retrieve frame set
+  #-? A frameset is a packet with 1 frame from each enabled stream (depth, color, gyro, accel)
+  frameset = pipe.wait_for_frames()
+  
+  #-> Get color frame from framset
+  color_frame = frameset.get_color_frame()
 
-    if cv.waitKey(10) & 0xFF == ord("q"):
+  #-> Get the actual frame that is the frame data
+  #-? You can get a lot of thing from a frame : metadata, data size, timestamp etc.
+  frame = np.asanyarray(color_frame.get_data())
+
+  #-> Swap RED and BLUE channels
+  #-? Intel returns BGR, opencv uses RGB by default
+  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  cv2.imshow("frame.png", frame)
+
+  #-> Window closure handler
+  if cv2.waitKey(10) & 0xFF == ord("q"):
         break
-cam.release()
+
+#-> Cleanup
+pipe.stop()
