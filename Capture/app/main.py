3989af2ce2e3a2128b87989e31853afce1df2862
Louis-Marie <louis-marie.restout@gmail.com>
2022-03-04 20:35:20 +0100

    Capture : Python Callback & Volume

--- Capture/app/main.py
+++ Capture/app/main.py
@@ -1,97 +1,127 @@
-import os                   # miscellaneous operating system interfaces
+import os
+import time
 import cv2                  # state of the art computer vision algorithms library
 import json as js           # JSON encoder and decoder
 import shutil as shu
-from cv2 import imshow        # high level file opertions
 import numpy as np          # fundamental package for scientific computing
 import pyrealsense2 as rs   # Intel RealSense cross-platform open-source API
-
-# -> Setup
-bus_path = "/Bus"
-root_path = os.path.join(bus_path, "data")
-save_path = os.path.join(root_path, "sample/sample1")
-
-if "data" in os.listdir(bus_path):
-    shu.rmtree(root_path)
-os.makedirs(os.path.join(save_path, "color"), exist_ok=True)
+from threading import Lock
 
 
-def record():
+
+img = cv2.Mat(np.zeros((1, 1)))
+previous_timestamp = -1.0
+image_ready = False
+lock = Lock()
+
+#-() Callback function called by pipeline when started
+def imu_callback(frame: rs.frame) -> None:
+    global previous_timestamp, image_ready, img
+    #-> If lock already in use - during frame usage - blocks this statement, otherwise blocks other statements
+    with lock:
+        #-> Convert to frameset
+        frameset = frame.as_frameset()
+        if frameset:
+            #-> Retrieve timestamp and ignore identical frameset based on their timestamp
+            current_timestamp = frameset.get_timestamp()
+            if abs(previous_timestamp - current_timestamp) < 0.001:
+                return
+                
+            #-> If frameset is new, retrieve its infrared frame and convert it to opencv Mat
+            infrared_frame = frameset.get_infrared_frame()
+            img = cv2.Mat(np.asanyarray(infrared_frame.get_data()))
+
+            #-> Convert timestamp from milliseconds to seconds
+            previous_timestamp = frameset.timestamp * 1e-3
+
+            #-> Turn on the flag for available image
+            image_ready = True
+
+
+def record() -> tuple:
+    global previous_timestamp, image_ready, img
+    #-> Setup
     pipe = rs.pipeline()
     cfg = rs.config()
-    cfg.enable_stream(rs.stream.color, format=rs.format.any, width=640, height=480, framerate=60)
-    pipe.start(cfg)
+    cfg.enable_stream(rs.stream.infrared, format=rs.format.y8,
+                      width=1280, height=720, framerate=30)
+    pipe.start(cfg, imu_callback)
 
-    # -> Skip 5 first frames to give the Auto-Exposure time to adjust
-    for _ in range(5):
-        pipe.wait_for_frames()
+    #-> Skip first frames to give the Auto-Exposure time to adjust
+    time.sleep(1)
 
     frames = []
     timestamps = []
     while True:
-        # -> Retrieve frame set
-        # -? A frameset is a packet with 1 frame from each enabled stream (depth, color, gyro, accel)
-        frameset = pipe.wait_for_frames()
+        #-> If lock already in use - during frame retrieval - blocks this statement, otherwise blocks other statements
+        with lock:
+            if image_ready:
+                timestamp = previous_timestamp
+                frame = img.copy()
+                image_ready = False
 
-        # -> Get color frame from framset
-        color_frame = frameset.get_color_frame()
+                timestamps.append(timestamp)
+                frames.append(frame)
 
-        # -> Get the actual frame that is the frame data
-        # -? You can get a lot of thing from a frame : metadata, data size, timestamp etc.
-        frame = np.asanyarray(color_frame.get_data())
-        timestamp = color_frame.get_timestamp()
+                cv2.imshow("Frame", frame)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
 
-        # -> Swap RED and BLUE channels
-        # -? Intel returns BGR, opencv uses RGB by default
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        frames.append(frame)
-        timestamps.append(timestamp)
-
-        cv2.imshow("frame.png", frame)
-
-        # -> Window closure handler
-        if cv2.waitKey(10) & 0xFF == ord("q"):
-            break
-    
+    #-> Cleanup
     pipe.stop()
     return frames, timestamps
 
 
-def saveJson(frames, timestamps):
+def saveJson(frames: list, timestamps: list) -> None:
+    #-> Path setup
+    bus_path = "/Bus"
+    root_path = os.path.join(bus_path, "data")
+    save_path = os.path.join(root_path, "sample/sample1")
+
+    if "data" in os.listdir(bus_path):
+        shu.rmtree(root_path)
+    os.makedirs(os.path.join(save_path, "color"), exist_ok=True)
+
+    #-> Camera device setup
     pipe = rs.pipeline()
     cfg = rs.config()
     cfg.enable_stream(rs.stream.color)
+
+    #-> Intrinsics retrieval
     profile = pipe.start(cfg)
     intrinsics = profile.get_stream(
         rs.stream.color).as_video_stream_profile().get_intrinsics()
     intrinsics = [[intrinsics.fx, 0,             intrinsics.ppx],
                   [0,             intrinsics.fy, intrinsics.ppy],
                   [0,             0,             1]]
+    
+    #-> Cleanup
     pipe.stop()
 
     frames_info = []
     for i in range(len(frames)):
-        # -> Prepare data : paths and fill frame info
+        #-> Prepare data : paths and fill frames info
         number = "0" * (8-len(str(i))) + str(i)
         frame_path = os.path.join(save_path, "color", number + ".jpg")
         frame_info = {"file_name_image": frame_path,
                       "timestamp": timestamps[i],
                       "intrinsics": intrinsics}
 
-        # -> Save data
+        #-> Save frames
         frames_info.append(frame_info)
-        imshow("frame captured", frames[i])
+        cv2.imshow("frame captured", frames[i])
         cv2.imwrite(frame_path, frames[i])
         if cv2.waitKey(10) & 0xFF == ord("q"):
             break
-
+    
+    #-> Save frames info file
     info = {"dataset": "sample",
             "path": "/Bus/sample",
             "scene": "sample1",
             "frames": frames_info}
     info_path = os.path.join(save_path, "info.json")
     js.dump(info, open(info_path, "w"))
+
 
 if __name__ == "__main__":
     saveJson(*record())
